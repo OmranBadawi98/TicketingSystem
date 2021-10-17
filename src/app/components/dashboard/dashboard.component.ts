680c12d0d42b724ee4a87f72ce91974c90d871ec
Omran <omranbadawi98@gmail.com>
2021-10-17 16:19:10 +0300

    change Size

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { TicketsService } from './../../core/services/tickets.service';
 import { Component, OnInit } from '@angular/core';
+import { BranchService } from 'src/app/core/services/branch.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(
+    private serviceBranch: BranchService,
+    private serviceTicket: TicketsService
+  ) {}
+  allBranches: number;
+  allTickets: number;
+  ngOnInit() {
+    this.serviceBranch.getData().subscribe((res) => {
+      this.allBranches = res.length;
+    });
+    this.serviceTicket.getData().subscribe((res) => {
+      this.allTickets = res.length;
+    });
+  }
 }
--- src/app/shared/pipes.pipe.ts
+++ src/app/shared/pipes.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'statusPip',
 })
+
 export class PipesPipe implements PipeTransform {
   transform(value: boolean, ...args: unknown[]): unknown {
     console.log(value, 'This Value');
